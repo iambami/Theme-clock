49d1cf95c6899bbb6a8978919ead3b8809a4818e
Atinuke Bami Kayode <titatinuks@gmail.com>
2021-07-16 18:46:43 +0100

    updated

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ toggle.addEventListener('click', (e) => {
     }
 })
 
+function setTime() {
+    const time = new Date();
+    const month =time.getMonth()
+    const day = time.getDay() 
+    const hours = time.getHours()
+    const hoursForClock = hours % 12
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`   
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`   
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`   
+}
+
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+setTime() 
+
+setInterval(setTime, 1000)
