ae41fb0e2905d2072d980541a46bcaf01b83b3ce
Atinuke Bami Kayode <titatinuks@gmail.com>
2021-07-16 19:37:08 +0100

    updated

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function setTime() {
     minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`   
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`  
     
-    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : `${seconds}`} ${ampm}`
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
     dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
 
@@ -44,6 +44,6 @@ function scale (number, inMin, inMax, outMin, outMax) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
 
-setTime() 
+setTime()  
 
 setInterval(setTime, 1000)
